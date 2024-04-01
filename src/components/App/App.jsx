c603e93aa33a66de2a0d6a2e1f1a63fdb5e1f8be
DSojat <rpush.it@gmail.com>
2024-04-01 13:25:09 +0300

    task1_2_3 no css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import userData from '/src/userData.json';
 import friends from '/src/friends.json';
+import transactions from '/src/transactions.json';
 
 import '../App/App.css';
 
@@ -19,6 +21,7 @@ function App() {
       <h2>Завдання 2 - Список друзів</h2>
       <FriendList friends={friends} />
       <h2>Завдання 3 - Історія транзакцій</h2>
+      <TransactionHistory items={transactions} />
     </>
   );
 }
